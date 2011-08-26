615643c21cf788492ac8a3e0e7b12989026fee72
Jeff Dutil <jdutil21@gmail.com>
2011-08-26 04:20:48 -0400

    Add sendgrid credentials.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,7 +4,10 @@ ContactUsDemo::Application.configure do
   ActionMailer::Base.smtp_settings = {
     :address        => "smtp.sendgrid.net",
     :port           => "25",
-    :authentication => :plain
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => ENV['SENDGRID_DOMAIN']
   }
   # The production environment is meant for finished, "live" apps.
   # Code is not reloaded between requests
